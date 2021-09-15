3437ff4b544e0556c7c97feddb49707f68e12fef
RicardoSzram <ricardo.szram.filho@gmail.com>
2021-09-15 19:36:38 -0300

    Execução em Background implementada

--- shell.c
+++ shell.c
@@ -6,6 +6,9 @@
 // Definicoes -----------------------------------------------------------
 #define TOKEN_AMNT 16
 #define TOKEN_DELIM " \t\r\n"
+#define TOKEN_BACK "&"
+
+int isBackground = 0;
 
 // Implementacoes -------------------------------------------------------
 void **realloc_buffer(void **buff, int new_size)
@@ -40,11 +43,15 @@ char **parse_line(char *line)
   char **tokens = malloc(TOKEN_AMNT * sizeof(char *)), *token;
 
   // TODO: checar possiveis erros de alocacao no malloc
-
   token = strtok(line, TOKEN_DELIM);
 
   while (token != NULL)
   {
+    if (strcmp(token, "&") == 0) {
+      isBackground = 1;
+      break;
+    }
+
     tokens[i] = token;
     i++;
 
@@ -83,15 +90,18 @@ int execute_commands(char **args)
     exit(1);
 
   } else { // este é o processo pai
-
-    // processo pai vai esperar pelo filho
-    pid_filho=wait(&status);
-
-    if(WIFEXITED(status))
-      printf("%d terminou com status %d\n",(int)pid_filho,WEXITSTATUS(status));
-    else
-      printf("Filho não terminou normalmente\n");
+    // Se o processo for executado em Background terá a global variable isBackground, então o pai não deve esperar
+    if ( isBackground == 0 ) {
+      // processo pai vai esperar pelo filho
+      pid_filho=wait(&status);
+      if(WIFEXITED(status))
+        printf("%d terminou com status %d\n",(int)pid_filho,WEXITSTATUS(status));
+      else
+        printf("Filho não terminou normalmente\n");
+    }
   }
+
+  isBackground = 0;
 
   return 0;
 }
